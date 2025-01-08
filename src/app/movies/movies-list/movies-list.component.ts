423777b8db5cd1ee6b0ec94333efd5bd9acf0787
Jesús Mateo <erazojesusmateo@gmail.com>
2025-01-08 18:36:26 -0500

    I change the design of movies list component

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,39 +1,16 @@
-import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { GenericListingComponent } from "../../shared/components/generic-listing/generic-listing.component";
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { RatingComponent } from "../../shared/components/rating/rating.component";
 
 @Component({
   selector: 'app-movies-list',
-  imports: [DatePipe, CurrencyPipe, UpperCasePipe, GenericListingComponent, MatButtonModule, MatIconModule, RatingComponent],
+  imports: [GenericListingComponent, MatButtonModule, MatIconModule],
   templateUrl: './movies-list.component.html',
   styleUrl: './movies-list.component.css'
 })
-export class MoviesListComponent implements OnInit {
-  ngOnInit(): void {
-    
-  }
-
+export class MoviesListComponent {
   title = 'angular-movies'
   @Input({required:true})
   movies! : any[]
-  addMovie = ()=>{
-    this.movies.push({
-      title: "Default Movie",
-      releaseDate: new Date('2025-01-12'),
-      ticketValue: 327
-    })
-  }
-
-  removeMovie = (movie:any) =>{
-    const index = this.movies.findIndex( (currentMovie: any) => currentMovie.title === movie.title)
-    this.movies.splice(index,1)
-  }
-
-  ratingValueHandler = (ratingValue: number) => {
-    alert(`The selected rating is ${ratingValue}`)
-  }
-
 }
